591be990fe01758ac9f74bb8f33a08cb644eda8f
shusovan-69 <shusovandutta153@gmail.com>
2026-07-13 10:57:11 +0530

    version 1.0

---
+++ BMI_calculator.py
@@ -0,0 +1,6 @@
+Hight=float(input('Enter your hight in cm:'))
+Weight=float(input('Enter your weight in KG:'))
+hight=Hight/100
+BMI=Weight/(hight*hight)
+print('Your BMI is :',round (BMI,2))
+                             
